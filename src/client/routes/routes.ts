483ddf90fe1b02e7d8238506c043c4fb3ddb7688
Daniel Anca <daniel.anca@eggs.ro>
2024-02-27 22:07:50 +0200

    Fixed issue regarding "/contact" route.

--- src/client/routes/routes.ts
+++ src/client/routes/routes.ts
@@ -1,14 +1,13 @@
 import publicRoutes from "./publicRoutes";
 import adminRoutes from "./adminRoutes";
 import shopRoutes from "./shopRoutes";
-import gameRoutes from "./gameRoutes";
 import { publicRoutesType } from "./types";
 
 const routes : publicRoutesType[]  = [
     ...publicRoutes,
     // ...adminRoutes,
     // ...shopRoutes,
-    ...gameRoutes
+   
 ]
 
 export default routes;
